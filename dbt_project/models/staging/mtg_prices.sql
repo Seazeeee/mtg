b98ffb61990dddec830d6cdbc8009b5336301137
Matthew Thompson <matthewthompson1113@gmail.com>
2024-10-23 00:05:25 -0400

    Changed price to a varchar so that metabase could read view.

--- dbt_project/models/staging/mtg_prices.sql
+++ dbt_project/models/staging/mtg_prices.sql
@@ -1,6 +1,6 @@
 SELECT  date, 
         name,
-        CAST(JSON_EXTRACT(prices, 'usd') AS DECIMAL(10,2)) AS usd_prices, 
+        CAST(JSON_EXTRACT(prices, 'usd') AS VARCHAR(255)) AS usd_prices, 
         type_line,
         CAST(JSON_EXTRACT(image_uris, 'normal') AS VARCHAR(2000)) as image
 FROM scryfall_data
