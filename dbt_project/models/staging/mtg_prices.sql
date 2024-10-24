02d777e14fcba5147329837fe77f1dbd436f086b
Matthew Thompson <matthewthompson1113@gmail.com>
2024-10-24 01:02:12 -0400

    Added correct source syntax for proper dependencies..

--- dbt_project/models/staging/mtg_prices.sql
+++ dbt_project/models/staging/mtg_prices.sql
@@ -3,4 +3,4 @@ SELECT  date,
         CAST(JSON_EXTRACT(prices, '$.usd') AS FLOAT) AS usd_prices, 
         type_line,
         CAST(JSON_EXTRACT(image_uris, '$.normal') AS VARCHAR(2000)) as image
-FROM scryfall_data
+FROM {{ source('main', 'scryfall_data') }}
